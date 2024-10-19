3cdac78df80501f95813a2d58c242c886ed0722d
Per Bergland <per@refapp.se>
2024-10-19 14:53:22 +0200

    more logging

--- server/main.js
+++ server/main.js
@@ -3,5 +3,11 @@ import { tlaValue } from "./tla-file";
 
 Meteor.startup(()=>{
     console.log("Starting!")
-    console.dir("Should show tlaValue: 4",{tlaValue});
+    console.log("Should show { tlaValue: 4 } and not { tlaValue: undefined }");
+    if (tlaValue === undefined) {
+        console.error({ tlaValue });
+    }
+    else {
+        console.log({ tlaValue });
+    }
 });
